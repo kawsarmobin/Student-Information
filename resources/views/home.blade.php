12f929623c0ae92c3bd6d7c00d9e629cd6e9888c
kawsar <hmkawsar559@gmail.com>
2018-10-08 13:34:25 +0600

    git init and templete setup complete

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Dashboard</h3>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <h4 style="text-align: center">
+                        You are logged in Mr/Mrs - <br>
+                        {{ ucwords(Auth::user()->name) }}
+                    </h4>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
